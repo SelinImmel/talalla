a82ceb3d95efe83cd95faed9f4cf706c295b88bc
Richard Angapin <rangapin@gmx.com>
2019-06-05 18:01:10 +0800

    personaldashinit

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,7 @@ Rails.application.routes.draw do
   resources :lessons, except: [:index] do
     resources :bookings, only: [:create]
   end
+
+  resources :user_plans, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
