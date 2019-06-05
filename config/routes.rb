9e1734e4a1cbd6fbfef0ae4d1597a4bfbc45831f
Nicolas Klug <nicolasklug@gmail.com>
2019-06-05 14:20:45 +0800

    added the show page of each event aswell as the routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :studios, only: [:show] do
     resources :lessons, only: [:index]
-    resources :events, only: [:index, :new, :create]
+    resources :events, only: [:index, :new, :create, :show]
     resources :subscriptions, only: [:index, :new, :create]
   end
 
