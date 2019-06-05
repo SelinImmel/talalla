a32261b437d50849353a442880ab000ea6bf77c3
Selin Immel <selin.immel@whu.edu>
2019-06-05 17:45:33 +0800

    added photo-mounter to events model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_084837) do
+ActiveRecord::Schema.define(version: 2019_06_05_094325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_06_05_084837) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "photo"
     t.index ["studio_id"], name: "index_events_on_studio_id"
   end
 
