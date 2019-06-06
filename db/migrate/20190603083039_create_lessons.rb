f610f138df56eb77b8183eb3fc2e772b38cea413
Nicolas Klug <nicolasklug@gmail.com>
2019-06-06 10:55:46 +0800

    changed the setting up of the lessons table as datetime instead of time

--- db/migrate/20190603083039_create_lessons.rb
+++ db/migrate/20190603083039_create_lessons.rb
@@ -7,9 +7,9 @@ class CreateLessons < ActiveRecord::Migration[5.2]
       t.string :location
       t.text :content
       t.integer :slots
-      t.time :time
+      t.datetime :start_time
+      t.datetime :end_time
       t.string :occurrence
-
       t.timestamps
     end
   end
