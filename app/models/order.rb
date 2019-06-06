b1d6614eaa775ed188e77739ed2a97d16835e594
Alina Heimgartner <heimgartner.alina@gmail.com>
2019-06-06 16:48:21 +0800

    changed registration form, updated admin view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :subscription
   belongs_to :user
+  has_one :user_plan, dependent: :destroy
 end
--- app/models/user_plan.rb
+++ app/models/user_plan.rb
@@ -1,5 +1,5 @@
 class UserPlan < ApplicationRecord
   belongs_to :order
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 end
