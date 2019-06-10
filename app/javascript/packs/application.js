94d8cc6d9dadfb65463116855accb419594b0efb
Alina Heimgartner <heimgartner.alina@gmail.com>
2019-06-10 17:28:11 +0800

    bug fixing for calendar, added booking to calendar and added missing rows into admin subscritpion and student

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+
+
+
+let lessonCells = document.querySelectorAll(".booked_lesson")
+if (lessonCells) {lessonCells.forEach((cell) => {
+   cell.parentElement.classList.add("calendar_bookings")
+  });
+}
