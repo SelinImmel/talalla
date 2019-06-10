3eb11d7e4fa0bde1c480f24d71a15ef76e494463
Nicolas Klug <nicolasklug@gmail.com>
2019-06-10 14:13:27 +0800

    booking vs buying logic implemented. user without susbscription is asked to buy a subscription, otherwise book button is displayed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,8 +17,8 @@ class BookingsController < ApplicationController
     @booking = Booking.new
 
     @lesson = Lesson.find(params[:lesson_id])
-    if @lesson.start_date <= current_user.user_plans.first.end_date && current_user.can_make_bookings? && @lesson.can_be_booked?
-      @booking.user_plan = current_user.user_plans.first
+    if @lesson.start_date <= current_user.user_plans.last.end_date && current_user.can_make_bookings? && @lesson.can_be_booked?
+      @booking.user_plan = current_user.user_plans.last
       @booking.lesson = @lesson
       @booking.user = current_user
 
@@ -28,7 +28,14 @@ class BookingsController < ApplicationController
         render :new
       end
     else
-      render :new
+      @lesson.start_date <= current_user.user_plans.last.end_date && current_user.can_make_bookings? && @lesson.can_be_booked?
+      @booking.user_plan = current_user.user_plans.last
+      @booking.lesson = @lesson
+      @booking.user = current_user
+
+      if @booking.save
+        redirect_to personal_dashboard_path
+      end
     end
   end
 
@@ -37,6 +44,5 @@ class BookingsController < ApplicationController
   def set_user
     @user = current_user
   end
-
 
 end
